485524bac27d8b0ec98ddf4b9c5adb5bb7239343
duddk1987 <43901659+duddk1987@users.noreply.github.com>
2020-04-15 11:27:27 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+$(document).on('click','a[href="#"]', function(e){
+    e.preventDefault();
+});
+
+
+// gnb menu 
+$(function(){
+    $('header .menuopen').on('click', function(){
+        $('.gnb').addClass('on');
+        });
+        $('.gnb .close, section').on('click',function(){
+            $('.gnb').removeClass('on');
+    });
+});
+
+
+
+// fix header
+var scrollTop = 0;
+scrollTop =$(document).scrollTop();
+fixHeader();
+
+// when you control window size... 
+$(window).on('scroll resize',function(){
+    scrollTop = $(document).scrollTop();
+    fixHeader();
+});
+
+//fixedheader function 
+function fixHeader(){
+    if(scrollTop > 150){
+        $('header').addClass('on');
+    } else {
+        $('header').removeClass('on');
+    }
+}
+
+
+// scroll animation scroll 할떄 애니메이션 되는 효과..
+$(function(){
+    $('.animate').scrolla({
+        mobile: false,
+        once: false
+    });
+});
+
+// go to top script
+$(function(){
+    $('.gotop').on('click',function(){
+        $('html,body').animate({
+            scrollTop : 0}, 2000);
+    });
+});
+
+
+//  hero img slide animation 
+$(function(){
+    $(".hero .slide").slick({
+        arrow: true, //화살표
+        dots: false, // 하단에 paging 버튼 노출
+        fade: true,  // fade in effect
+        autoplay: true, // 자동으로 플레이 될건지
+        autoplaySpeed: 4000, //다음이미지로 넘어갈 시간
+        pauseOnHover: false,
+        pauseOnFocus: false
+    }); 
+});
+
+
